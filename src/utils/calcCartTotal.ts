5c761edc9c88723115e7704cd9e23dc790fbbdb6
Dennis Dovziy <1044442@gmail.com>
2022-11-02 15:41:56 +0200

    cart bug fixed

--- src/utils/calcCartTotal.ts
+++ src/utils/calcCartTotal.ts
@@ -1,6 +1,7 @@
 import { CartItemType } from '../store/cart/types';
 
 export const calcCartTotalPrice = (items: CartItemType[]) => {
+    console.log(items);
     return items.reduce((sum, obj) => obj.price * obj.count + sum, 0);
 };
 
--- src/utils/getLocalStorageCart.ts
+++ src/utils/getLocalStorageCart.ts
@@ -2,9 +2,9 @@ import { calcCartTotalCount, calcCartTotalPrice } from './calcCartTotal';
 
 export const getLocalStorageCart = () => {
     const cartData = localStorage.getItem('foodCourt-cart');
-    const storageData = cartData ? JSON.parse(cartData) : {};
-    const items = storageData ? storageData.items : [];
-    const groups = storageData ? storageData.groups : [];
+    const storageData = cartData ? JSON.parse(cartData) : [];
+    const items = storageData.items ? storageData.items : [];
+    const groups = storageData.groups ? storageData.groups : [];
     const totalPrice = calcCartTotalPrice(items) || 0;
     const totalCount = calcCartTotalCount(items) || 0;
 
